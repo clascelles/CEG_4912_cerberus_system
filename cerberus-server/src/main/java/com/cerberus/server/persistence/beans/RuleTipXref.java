d9b9c506e1c1fbecd3284e8b371d51b815aee0d0
mike <michael.woods104@gmail.com>
2013-03-22 09:25:12 -0400

    Rule, Tip, RuleTipXref beans

---
+++ cerberus-server/src/main/java/com/cerberus/server/persistence/beans/RuleTipXref.java
@@ -0,0 +1,57 @@
+package com.cerberus.server.persistence.beans;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "RULE_TIP_XREF")
+public class RuleTipXref implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+	Integer id;
+	Rule rule;
+	Tip tip;
+	
+	public RuleTipXref(Rule rule, Tip tip) {
+		super();
+		this.rule = rule;
+		this.tip = tip;
+	}
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="ID", nullable=false)
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	@Column(name="RULE_ID", nullable=false)
+	public Rule getRule() {
+		return rule;
+	}
+	public void setRule(Rule rule) {
+		this.rule = rule;
+	}
+	
+	@Column(name="TIP_ID", nullable=false)
+	public Tip getTip() {
+		return tip;
+	}
+	public void setTip(Tip tip) {
+		this.tip = tip;
+	}	
+	
+}
