ab8b8143a8646cc3ed83b0fbc4c7d42e72274385
Charles Lascelles <charles.lascelles@gmail.com>
2013-03-26 09:54:51 -0400

    Message class is no longer abstract (cannot be if we want to deserialize a Json message)

--- cerberus-server/src/main/java/com/cerberus/server/message/Message.java
+++ cerberus-server/src/main/java/com/cerberus/server/message/Message.java
@@ -2,21 +2,19 @@ package com.cerberus.server.message;
 
 import org.joda.time.DateTime;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonSubTypes.Type;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
-@JsonSubTypes({
- @Type(value = RFIDAuthRequestMessage.class, name = "RFID_AUTH_REQ"),
+@JsonSubTypes({ @Type(value = RFIDAuthRequestMessage.class, name = "RFID_AUTH_REQ"),
 		@Type(value = CurrentConsumptionMessage.class, name = "CURRENT"),
 		@Type(value = StatusMessage.class, name = "STATUS"),
 		@Type(value = RFIDAuthResponseMessage.class, name = "RFID_AUTH_RES"),
 		@Type(value = SwitchOperatingModeMessage.class, name = "SWITCH_OP_MODE"),
 		@Type(value = GenericInformationMessage.class, name = "GENERIC_INFO") })
-public abstract class Message {
+public class Message {
 
 	public enum MessageType {
 		RFID_AUTH_REQ, CURRENT, STATUS, RFID_AUTH_RES, SWITCH_OP_MODE, GENERIC_INFO
@@ -26,7 +24,6 @@ public abstract class Message {
 	private final int socketId;
 	private final DateTime timestamp;
 
-	@JsonCreator
 	public Message(@JsonProperty("type") MessageType type, @JsonProperty("socketId") int socketId,
 			@JsonProperty("timestamp") long timestamp) {
 		this.type = type;
