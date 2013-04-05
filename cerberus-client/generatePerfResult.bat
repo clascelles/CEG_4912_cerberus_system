25da001ef67e2e2c7ead2d37153a47a5bee3f994
Charles Lascelles <charles.lascelles@gmail.com>
2013-04-04 22:44:09 -0400

    Made two batch files to generate performance stats and graphs - Hopefully they are generic enough and should work on your machine, let me know if there's a problem with those two.

---
+++ cerberus-client/generatePerfResult.bat
@@ -0,0 +1,7 @@
+@echo off
+FOR /F "usebackq tokens=1,2,3,4 delims=/ " %%i IN (`date /t`) DO (set yyyymmdd=%%l%%k%%j)
+set jar_version=0.9.16
+set jar_path=%USERPROFILE%\.m2\repository\org\perf4j\perf4j\%jar_version%\perf4j-%jar_version%.jar
+set project_path=%USERPROFILE%\workspace\git\CEG_4912_cerberus_system\cerberus-client
+java -jar %jar_path% %project_path%\logs\perflog_%yyyymmdd%.txt > %project_path%\perfResults.txt
+pause
