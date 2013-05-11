6ac1f9c9c6c53ceb6117dd9939d840c27f19d886
Charles Lascelles <charles.lascelles@gmail.com>
2013-05-11 15:30:04 -0400

    Replaced log4j.properties for log4j.xml for automatic graph generation

--- cerberus-server/src/main/java/com/cerberus/server/bootstrap/CerberusServerBootstrap.java
+++ cerberus-server/src/main/java/com/cerberus/server/bootstrap/CerberusServerBootstrap.java
@@ -1,17 +1,17 @@
 package com.cerberus.server.bootstrap;
 
 import org.apache.log4j.Logger;
-import org.apache.log4j.PropertyConfigurator;
+import org.apache.log4j.xml.DOMConfigurator;
 
 public class CerberusServerBootstrap extends Thread {
 
-	private final static String LOG4J_PROPERTIES = "src/main/resource/log4j.properties";
+	private final static String LOG4J_XML = "src/main/resource/log4j.xml";
 	private final static Logger LOGGER = Logger.getLogger(CerberusServerBootstrap.class);
 
 	@Override
 	public void run() {
 
-		PropertyConfigurator.configure(LOG4J_PROPERTIES);
+		DOMConfigurator.configure(LOG4J_XML);
 
 		LOGGER.info("Bootstrapping the Cerberus server.");
 		// Start all bootstrap threads here
