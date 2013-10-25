24bae82fc255dc354810c443e397309f4e72a200
Charles Lascelles <charles.lascelles@gmail.com>
2013-10-25 09:07:43 -0400

    Removed legacy ties with the JSON messaging library

--- webappserver/src/main/java/com/cerberus/daemon/constants/SocketOperatingMode.java
+++ webappserver/src/main/java/com/cerberus/daemon/constants/SocketOperatingMode.java
@@ -1,8 +1,5 @@
 package com.cerberus.daemon.constants;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonValue;
-
 public enum SocketOperatingMode {
 
 	ON(1), OFF(2), MONITORING(3), SAFETY(4), RESTRICTED(5);
@@ -13,12 +10,10 @@ public enum SocketOperatingMode {
 		this.opMode = opMode;
 	}
 
-	@JsonValue
 	public int getIntValue() {
 		return opMode;
 	}
 
-	@JsonCreator
 	public static SocketOperatingMode fromIntValue(int opModeValue) {
 		for (SocketOperatingMode opMode : SocketOperatingMode.values()) {
 			if (opModeValue == opMode.getIntValue()) {
