f8b7af5a435a6d30db803094ad4e2c1bceaff007
Charles Lascelles <charles.lascelles@gmail.com>
2013-10-28 11:47:58 -0400

    Clean-up for UsageController

--- webappserver/src/main/java/com/cerberus/module/usage/controllers/UsageController.java
+++ webappserver/src/main/java/com/cerberus/module/usage/controllers/UsageController.java
@@ -1,7 +1,5 @@
 package com.cerberus.module.usage.controllers;
 
-import java.util.List;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,38 +9,35 @@ import com.cerberus.frameworks.spring.CerberusApplicationContext;
 import com.cerberus.model.account.bean.User;
 import com.cerberus.module.generic.constants.CerberusConstants;
 import com.cerberus.module.generic.controllers.CerberusController;
-import com.cerberus.module.outlets.workflows.OutletWorkflow;
-import com.cerberus.module.overview.backingobjects.TopBarBackingObject;
 import com.cerberus.module.usage.constants.UsageConstants;
 import com.cerberus.module.usage.workflows.UsageWorkflow;
 
 @Controller
 public class UsageController extends CerberusController {
-	
+
 	@RequestMapping(value=CerberusConstants.USAGE_VIEW, method=RequestMethod.GET)
 	public String getLoginPage(Model model)	{
-		
-		User user = getUser();		
+
+		User user = getUser();
 		if(user == null){
 			return CerberusConstants.REDIRECT;
-		}		s
+		}
 		initTopBar(model, user);
-		s
-		
+
 		UsageWorkflow usageWorkflow = CerberusApplicationContext.getWorkflows().getUsageWorkflow();
-		
+
 		//Get current comsumption per hour for the last 24 hours.
 		Integer[] currentHourList = usageWorkflow.getCurrentByHourForLast24h(user);
-		
+
 		model.addAttribute(UsageConstants.CURRENT_HOUR_LIST, currentHourList);
-		
+
 		return "usage/index";
 	}
-	
+
 	@RequestMapping(value="/usage/index", method=RequestMethod.POST)
 	public String post(Model model)	{
 		return null;
-		
+
 	}
 
 }
