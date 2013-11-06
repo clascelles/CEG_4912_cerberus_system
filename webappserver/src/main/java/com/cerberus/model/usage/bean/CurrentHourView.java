feeff9485b82b86969a840a4fb86be32b89ffeaa
David <david.vezina@hotmail.com>
2013-11-06 11:23:21 -0500

    Added missing sql.Timestamp import

--- webappserver/src/main/java/com/cerberus/model/usage/bean/CurrentHourView.java
+++ webappserver/src/main/java/com/cerberus/model/usage/bean/CurrentHourView.java
@@ -2,6 +2,7 @@ package com.cerberus.model.usage.bean;
 
 import java.io.Serializable;
 import java.sql.Date;
+import java.sql.Timestamp;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
