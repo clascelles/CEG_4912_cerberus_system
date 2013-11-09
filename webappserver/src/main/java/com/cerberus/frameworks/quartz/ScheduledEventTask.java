f68c2f6fad5eccb34ebc7a500d23451ac2bb4a16
mike <michael.woods104@gmail.com>
2013-11-09 18:03:51 -0500

    Scheduling message hook implementation

--- webappserver/src/main/java/com/cerberus/frameworks/quartz/ScheduledEventTask.java
+++ webappserver/src/main/java/com/cerberus/frameworks/quartz/ScheduledEventTask.java
@@ -4,6 +4,9 @@ import java.util.Date;
 
 import org.apache.log4j.Logger;
 
+import com.cerberus.daemon.constants.OperationModeUtil;
+import com.cerberus.frameworks.spring.CerberusApplicationContext;
+import com.cerberus.model.outlets.bean.Socket;
 import com.cerberus.model.outlets.bean.SocketOperationMode;
 import com.cerberus.model.schedules.bean.ScheduleRecurrence;
 import com.cerberus.model.schedules.bean.ScheduledEvent;
@@ -11,7 +14,7 @@ import com.cerberus.model.schedules.bean.ScheduledEvent;
 public class ScheduledEventTask {
 
 	private Integer eventId;
-	private Integer socketId;
+	private Socket socket;
 	private SocketOperationMode mode;
 	private Date timestamp;
 	private ScheduleRecurrence recurrence;
@@ -23,7 +26,7 @@ public class ScheduledEventTask {
 	
 	public void init(ScheduledEvent event) {
 		this.eventId = event.getId();
-		this.socketId = event.getSocket().getId();
+		this.socket = event.getSocket();
 		this.mode = event.getMode();
 		this.timestamp = event.getTime();
 		this.recurrence = event.getRecurrence();
@@ -33,7 +36,7 @@ public class ScheduledEventTask {
 		
 		System.out.println("Running scheduled event task....");
 		System.out.println("Event id: " + eventId);
-		System.out.println("Socket id: " + socketId);
+		System.out.println("Socket id: " + socket.getId());
 		System.out.println("Socket operation mode id: " + mode.getDescription());
 		System.out.println("Timestamp: " + timestamp);
 		System.out.println("Recurrence level: " + recurrence);
@@ -42,7 +45,13 @@ public class ScheduledEventTask {
 		
 	}
 	
-	public static void sendMessage() {
-		//TODO
+	public void sendMessage() {
+		int position = socket.getPosition();
+		CerberusApplicationContext.getWorkflows().getSwitchOperationModeWorkflow().
+				sendMessage(socket.getOutlet().getSerialNumber(), 
+						position, 
+						"0000000000",
+						OperationModeUtil.getSocketOperatingMode(socket.getOutlet().getMode(), socket.getMode()), 
+						0);
 	}
 }
