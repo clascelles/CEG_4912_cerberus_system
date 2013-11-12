860bad6dcaf753e04fa76affaecd3e7eff447e5d
Charles Lascelles <charles.lascelles@gmail.com>
2013-11-11 21:27:34 -0500

    Fixed CurrentConsumptionWorkflow to properly handle current values

--- webappserver/src/main/java/com/cerberus/daemon/workflow/CurrentConsumptionWorkflow.java
+++ webappserver/src/main/java/com/cerberus/daemon/workflow/CurrentConsumptionWorkflow.java
@@ -1,32 +1,39 @@
 package com.cerberus.daemon.workflow;
 
+import java.sql.Timestamp;
+
 import org.apache.log4j.Logger;
 import org.perf4j.StopWatch;
 import org.perf4j.log4j.Log4JStopWatch;
 
+import com.cerberus.daemon.message.CurrentConsumptionMessage;
 import com.cerberus.daemon.message.Message;
 import com.cerberus.daemon.message.WrongMessageException;
+import com.cerberus.model.outlets.bean.Current;
+import com.cerberus.model.outlets.bean.Outlet;
+import com.cerberus.model.outlets.bean.Socket;
+import com.cerberus.model.security.bean.RfidTag;
+import com.cerberus.service.outlets.OutletService;
+import com.cerberus.service.security.RfidService;
 
 public class CurrentConsumptionWorkflow extends MessageWorkflow {
 
 	// Get Logger
 	private final static Logger LOGGER = Logger.getLogger(CurrentConsumptionWorkflow.class);
 
-	//private final ServiceFactory serviceFactory;
-
 	public CurrentConsumptionWorkflow() {
 		super(false);
-		//serviceFactory = borrowServiceFactory();
-		//LOGGER.info("[CurrentWorkflow]: Initializing. Borrowing Service Factory from ObjectPool");
 	}
 
 	@Override
 	public boolean handleMessage(Message receivedMessage) throws WrongMessageException {
 
+		OutletService outletService = serviceFactory.getOutletService();
+		RfidService rfidService = serviceFactory.getRfidService();
+
 		StopWatch stopwatch = new Log4JStopWatch("CurrentWorkflow.handleReceivedMessage");
 
 		// Create new Current data structure
-		/*
 		CurrentConsumptionMessage currentMessage;
 		if (receivedMessage instanceof CurrentConsumptionMessage) {
 			currentMessage = (CurrentConsumptionMessage) receivedMessage;
@@ -37,20 +44,38 @@ public class CurrentConsumptionWorkflow extends MessageWorkflow {
 
 		Current current = new Current();
 
-		current.setTimestamp(new Timestamp(receivedMessage.getTimestamp()*1000));
+		current.setTimestamp(new Timestamp(currentMessage.getTimestamp()*1000));
 		current.setCurrent(currentMessage.getCurrent());
 
+		// Get outlet
+		Outlet outlet = outletService.getOutletBySerialNumber(currentMessage.getOutletId());
+		if (outlet == null) {
+			// Ignore current message since the outlet is not set in the database
+			return false;
+		}
+
 		// Set Socket
-		current.setSocket(serviceFactory.getOutletService().getSocketBySerialNumber(receivedMessage.getOutletId()));
+		Socket socket = outletService.getSocketFromOutlet(outlet.getId(), currentMessage.getSocket());
+
+		// Current values will be tied to the socket
+		if (socket != null) {
+			current.setSocket(socket);
+		} else {
+			// TODO:Log error!
+			return false;
+		}
 
 		// Set User
-		current.setUser(serviceFactory.getUserService().getUserBySocketId(serviceFactory.getOutletService().getSocketBySerialNumber(receivedMessage.getOutletId()).getId()));
+		//User user = userService.getUserBySocketId(socket.getId());
+
+		// Current values will not be tied to users!
+		current.setUser(null);
 
 		// RFID is optional
 		String rfidNumber = currentMessage.getRfidNumber();
 		if (rfidNumber != null) {
 			// Set RFID Number
-			RfidTag tag = serviceFactory.getRfidService().getRfidTagByNumber(rfidNumber);
+			RfidTag tag = rfidService.getRfidTagByNumber(rfidNumber);
 			current.setRfidTagId(tag);
 		} else {
 			current.setRfidTagId(null);
@@ -67,7 +92,6 @@ public class CurrentConsumptionWorkflow extends MessageWorkflow {
 		} finally {
 			stopwatch.stop();
 		}
-		*/
 
 		return true;
 	}
