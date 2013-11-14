6b78b593c7386644f3c0456904e0125f21e86c4c
Charles Lascelles <charles.lascelles@gmail.com>
2013-11-13 22:40:16 -0500

    Added inverted map for channel binding so that we can get the outlet serial number from the binded channel ID

--- webappserver/src/main/java/com/cerberus/frameworks/netty/ChannelOutletBinding.java
+++ webappserver/src/main/java/com/cerberus/frameworks/netty/ChannelOutletBinding.java
@@ -12,12 +12,17 @@ public class ChannelOutletBinding {
 
 	//Create a new HashMap with 1000 places
 	private static HashMap<String, Integer> map = new HashMap<String, Integer>(CHANNEL_ID_MAP_SIZE);
+	private static HashMap<Integer, String> invertMap = new HashMap<Integer, String>(CHANNEL_ID_MAP_SIZE);
 	private static ChannelGroup channelGroup = new DefaultChannelGroup();
 
 	public static int getChannelId(String outletSerialNumber){
 		return map.get(outletSerialNumber);
 	}
-	
+
+	public static String getOutletSerialNumber(Integer channelId) {
+		return invertMap.get(channelId);
+	}
+
 	public static boolean isChannelBinded(String outletSerialNumber){
 		if (map.get(outletSerialNumber) != null){
 			return true;
@@ -28,6 +33,7 @@ public class ChannelOutletBinding {
 
 	public static void bindOutletSerialNumberWithChannelId(String outletSerialNumber, Integer channelId){
 		map.put(outletSerialNumber, channelId);
+		invertMap.put(channelId, outletSerialNumber);
 	}
 
 	public static void addChannelToGroup(Channel channel){
