ecdbc6f1cb2cfa48bdd4c46f45c2a27b9f330442
Charles Lascelles <charles.lascelles@gmail.com>
2013-12-02 12:53:17 -0500

    Changed kWh to W/s

--- webappserver/src/main/java/com/cerberus/module/usage/workflows/UsageWorkflow.java
+++ webappserver/src/main/java/com/cerberus/module/usage/workflows/UsageWorkflow.java
@@ -43,7 +43,7 @@ public class UsageWorkflow extends Workflow {
 
 		return currentHourList;
 	}
-	
+
 	public double[] getSocketCurrentByHourForDay(Socket socket, Date selectedDate){
 
 		ConsumptionService consumptionService = serviceFactory.getConsumptionService();
@@ -75,48 +75,48 @@ public class UsageWorkflow extends Workflow {
 
 		return currentDayList;
 	}
-	
+
 	public double[] getCostSavingsForDay(User user, Date selectedDate){
 
 		double peakRate = 0.129;
 		double midPeakRate = 0.109;
 		double offPeakRate = 0.072;
-		
+
 		List<Integer> peak = new ArrayList<Integer>();
 		List<Integer> midPeak = new ArrayList<Integer>();
-		
+
 		peak.add(7); 	peak.add(8);	peak.add(9);	peak.add(10);
-		
-		midPeak.add(11); midPeak.add(12); midPeak.add(13); 
+
+		midPeak.add(11); midPeak.add(12); midPeak.add(13);
 		midPeak.add(14); midPeak.add(15); midPeak.add(16);
 
 		peak.add(17); peak.add(18);
-		
+
 		//Get the System that the user belongs
 		SystemService systemService = serviceFactory.getSystemService();
 		Integer systemId = systemService.getSystemIdByUserId(user.getId());
-		
+
 		ConsumptionService consumptionService = serviceFactory.getConsumptionService();
 		List<CurrentHourView> currentHourViewList = consumptionService.getCurrentHourForDay(systemId, selectedDate);
 
 		double[] costSavings = new double[2];
-		
+
 		//Add the current hour to the list and handle the potential missing values.
 		for(CurrentHourView currentHour : currentHourViewList){
 			Double cost = 0.0;
 			Double savings = 0.0;
 			Double usage = currentHour.getCurrentHour();
-			
+
 			// if weekend
 			if(currentHour.getTimestampHour().getDay() == 0 ||
 					currentHour.getTimestampHour().getDay() == 6) {
 
 				cost = usage * offPeakRate;
 				savings = usage * (peakRate - offPeakRate);
-				
+
 			// if weekday
 			} else {
-				
+
 				if(peak.contains(currentHour.getHour())) {
 					cost = usage * peakRate;
 				} else if(midPeak.contains(currentHour.getHour())) {
@@ -126,38 +126,38 @@ public class UsageWorkflow extends Workflow {
 					cost = usage * offPeakRate;
 					savings = usage * (peakRate - offPeakRate);
 				}
-				
-			}			
-			
+
+			}
+
 			costSavings[0] += cost;
 			costSavings[1] += savings;
 		}
-		
+
 		NumberFormat formatter = new DecimalFormat();
 		formatter.setMaximumFractionDigits(3);
 		costSavings[0] = Double.parseDouble(formatter.format(costSavings[0]));
-		costSavings[1] = Double.parseDouble(formatter.format(costSavings[1]));		
+		costSavings[1] = Double.parseDouble(formatter.format(costSavings[1]));
 
 		return costSavings;
-	}	
-	
+	}
+
 	public double[] getCostSavingsForMonth(User user, Date selectedDate){
-		
+
 		List<Date> dates = new ArrayList<Date>();
-		
+
 		for(int i=1; i<selectedDate.getDate()+1; i++) {
 			dates.add(new Date(selectedDate.getYear(), selectedDate.getMonth(), i, 0, 0));
 		}
-		
+
 		double[] costSavings = new double[2];
 		costSavings[0] = 0.0;
 		costSavings[1] = 0.0;
-		
+
 		for(Date date : dates) {
 			double[] cs = getCostSavingsForDay(user, date);
 			costSavings[0] += cs[0];
 			costSavings[1] += cs[1];
-		}	
+		}
 
 		return costSavings;
 	}
@@ -166,76 +166,76 @@ public class UsageWorkflow extends Workflow {
 		DateTime threshold = new DateTime(date.getTime()).minusDays(UsageConstants.KEEP_FOR_DAYS);
 		threshold = threshold.minusMinutes(threshold.getMinuteOfDay());
 		threshold = threshold.minusSeconds(threshold.getSecondOfMinute());
-		
+
 		ConsumptionService consumptionService = serviceFactory.getConsumptionService();
 
 		consumptionService.updateCurrentHour(threshold);
 		consumptionService.deleteCurrentByThreshold(threshold);
 	}
-	
+
 	public List<Tip> getTipsWithRules(){
 		ConsumptionService consumptionService = serviceFactory.getConsumptionService();
 		return consumptionService.getTipsWithRules();
 	}
-	
+
 	public List<Tip> getTipsWithoutRules(){
 		ConsumptionService consumptionService = serviceFactory.getConsumptionService();
 		return consumptionService.getTipsWithoutRules();
 	}
-	
+
 	public Current getCurrentForSocket(Socket socket) {
 		ConsumptionService consumptionService = serviceFactory.getConsumptionService();
 		return consumptionService.getMostRecentCurrentBySocketId(socket.getId());
 	}
-	
+
 	public String getCurrentUsageForSocket(Socket socket) {
 		Current current = getCurrentForSocket(socket);
 		if(current == null) {
-			return "0 kWh";
+			return "0 W/s";
 		} else {
-			return current.getCurrent() + " kWh";			
+			return current.getCurrent() + " W/s";
 		}
 	}
 
 	public List<Integer> matchCurrentUsage(DetachedCriteria criteria) {
 		ConsumptionService consumptionService = serviceFactory.getConsumptionService();
 		return consumptionService.listCurrent(criteria);
-		
+
 	}
-	
+
 	public List<Integer> getSystemList(List<Integer> currentList){
 		ConsumptionService consumptionService = serviceFactory.getConsumptionService();
 		return consumptionService.getSystemListFromCurrentList(currentList);
 	}
-	
+
 	public Double getCurrentTotalUsage(User user) {
 		OutletWorkflow outletWorkflow = CerberusApplicationContext.getWorkflows().getOutletWorkflow();
 		List<Socket> sockets = outletWorkflow.getSocketsByUser(user);
-		
+
 		ConsumptionService consumptionService = serviceFactory.getConsumptionService();
-		
+
 		Date oneMinuteAgo = new Date(new Date().getTime() - 1000*30);
-		
+
 		Double total = 0.0;
-		
+
 		for(Socket socket : sockets) {
 			List<Current> currents = consumptionService.getCurrentBySocketId(socket.getId());
-			
+
 			for(Current current : currents) {
 				if(current.getTimestamp().after(oneMinuteAgo)) {
 					total += current.getCurrent();
 				}
 			}
 		}
-		
-		return total;		
+
+		return total;
 	}
-	
+
 	public Integer insertSystemTip(Integer tipId, Integer systemId ){
 		ConsumptionService consumptionService = serviceFactory.getConsumptionService();
 		return consumptionService.insertSystemTip(new SystemTip(systemId, tipId));
 	}
-	
+
 	public List<Tip> getLatest10Tips(Integer systemId){
 		ConsumptionService consumptionService = serviceFactory.getConsumptionService();
 		List<Tip> tips = new ArrayList<Tip>(10);
@@ -245,12 +245,12 @@ public class UsageWorkflow extends Workflow {
 		}
 		return tips;
 	}
-	
+
 	public List<SystemTip> getLatest10SystemTips(Integer systemId){
 		ConsumptionService consumptionService = serviceFactory.getConsumptionService();
 		return consumptionService.getSystemTipList(systemId, 10);
 	}
-	
+
 	public List<Integer> getSystemIds(){
 		return serviceFactory.getSystemService().getAllSystemIds();
 	}
